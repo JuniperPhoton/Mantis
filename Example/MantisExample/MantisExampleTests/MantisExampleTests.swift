cb2d04414a0e5aec13801be79864237b97f51f9b
juniperphoton <dengweichao@hotmail.com>
2026-03-16 20:53:42 +0900

    Fix example proj

---
+++ Example/MantisExample/MantisExampleTests/MantisExampleTests.swift
@@ -0,0 +1,16 @@
+//
+//  MantisExampleTests.swift
+//  MantisExampleTests
+//
+//  Created by juniperphoton on 3/16/26.
+//
+
+import Testing
+
+struct MantisExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
